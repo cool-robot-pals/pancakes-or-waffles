fc4bf96f567bc2a9ca3a747fd2229e7ec1744dcb
Laura González <lawr@lauragonzalez.cc>
2017-01-10 00:55:29 +0100

    neater

--- src/lib/photoGetter.js
+++ src/lib/photoGetter.js
@@ -11,7 +11,6 @@ module.exports = function(query,params) {
 	var dfd = jQuery.Deferred();
 	if(params.debug === true) {
 		dfd.resolve([
-			'https://i.ytimg.com/vi/R45OaTeR_Gw/maxresdefault.jpg',
 			'https://i.ytimg.com/vi/R45OaTeR_Gw/maxresdefault.jpg'
 		]);
 	}
@@ -30,9 +29,12 @@ module.exports = function(query,params) {
 				key: env.googleSearchKey,
 			}
 		}).done(function(response){
+
+			var length = 15;
+			if(response.items.length < 15) length = response.items.length;
+
 			dfd.resolve([
-				response.items[Math.floor(Math.random() * 15)].link,
-				response.items[Math.floor(Math.random() * 15)].link
+				response.items[Math.floor(Math.random() * length)].link
 			]);
 		});
 	}
