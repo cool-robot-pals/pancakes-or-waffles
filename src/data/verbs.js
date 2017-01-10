07efa170dad0381e32e0062152e9067e3e559278
Laura González <lawr@lauragonzalez.cc>
2017-01-10 01:27:19 +0100

    cleanup

--- src/data/verbs.js
+++ src/data/verbs.js
@@ -1,3 +1,3 @@
 module.exports = [
-	'save','kill','kiss','sacrifice','pet','eat','join','forgive','tell','destroy','choose','pardon','evict','visit','read with','watch with','travel with','eat $1 with','rescue','cure','heal','boop','draw','give $1 to','entertain'
+	'save','kill','kiss','sacrifice','pet','eat','join','forgive','tell','destroy','choose','pardon','evict','visit','read with','watch with','travel with','eat $1 with','rescue','cure','heal','boop','give $1 to'
 ];
