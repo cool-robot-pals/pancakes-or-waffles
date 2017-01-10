76b4c946a777c1e536e08136171df6204f372735
Laura González <lawr@lauragonzalez.cc>
2017-01-10 01:27:19 +0100

    cleanup

--- src/data/things.js
+++ src/data/things.js
@@ -1,1 +1,1 @@
-module.exports = ['waffles','pancakes','videogames','Arcadia Bay','gays','Himiko','the truth','the Mushroom Kingdom','Hyrule','memes','Beach City','this cat','this dog','the Triforce','bacon','halloween','anime','the journal','the blue key','eggs','goombas'];
+module.exports = ['waffles','pancakes','videogames','Arcadia Bay','gays','Himiko','the truth','the Mushroom Kingdom','Hyrule','memes','Beach City','this cat','this dog','the Triforce','bacon','halloween','anime','the journal','eggs','goombas'];
