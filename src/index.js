409d7e18595cbd97368b7032feb9f4335f3377ec
Laura González <lawr@lauragonzalez.cc>
2017-01-11 12:20:47 +0100

    Vast improvements

--- src/index.js
+++ src/index.js
@@ -124,7 +124,17 @@ var makePost = function() {
 
 	var $post = $('<post></post>');
 
-	var layout = random([0,1,2,3,4]);
+	/*
+	0 - lis - serious
+	1 - lis - normal
+	2 - bioshock
+	3 - fallout
+	4 - zelda
+	5 - saints row-ish
+	6 - quantic dream-ish
+	*/
+
+	var layout = random([0,1,3,4,5,6]);
 	var choices = makeChoices();
 
 	if(!query) {
