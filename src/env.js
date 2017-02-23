b32762cab8a100a88173eee9d65185ac39a6b028
Laura González <lawr@lauragonzalez.cc>
2017-02-23 15:44:49 +0100

    😇

---
+++ src/env.js
@@ -0,0 +1,8 @@
+module.exports = {
+	twitterConsumerKey: process.env.MC_TWITTER_APPK,
+	twitterConsumerSecret: process.env.MC_TWITTER_APPS,
+	twitterAccess: process.env.MC_TWITTER_AT,
+	twitterSecret: process.env.MC_TWITTER_S,
+	googleSearchCx: process.env.MC_GOOGLE_CX,
+	googleSearchKey: process.env.MC_GOOGLE_KEY
+};
