e701a4c15d3c8ab823c1815d5132f06b75a9066b
Lau Skeeter <lawr@lauragonzalez.cc>
2017-03-13 19:45:59 +0100

    expandable narrator

--- src/lib/txtToArr.js
+++ src/lib/txtToArr.js
@@ -1,10 +1,55 @@
+const propsregex = /\((.*?)\)/;
+const exploderegex = /\[(.*?)\]/;
+
+const explodeChunkVariables = (chunk) => {
+	let rt = [];
+	let exploded;
+	if(typeof chunk === 'object') {
+		chunk.map(innerChunk => {
+			rt = rt.concat(explodeChunkVariables(innerChunk));
+		});
+		return rt;
+	}
+	else {
+		try {
+			exploded = exploderegex.exec(chunk)[1].split(',');
+		}
+		catch(err){
+			return rt;
+		}
+		if(exploded){
+			rt = [];
+			exploded.map(word => {
+				rt.push(
+					chunk.replace(exploderegex,word.trim())
+				)
+			})
+		}
+		return rt;
+	}
+}
+
 export default (str) => {
-	const propsregex = /\((.*)?\)/;
-	return str
+	let array = str
 		.split('\n')
 		.map(chunk => chunk.trim())
 		.filter(chunk => chunk.charAt(0) !== '#')
 		.filter(chunk => chunk.length > 0)
+		.map(chunk => {
+			if(chunk === '_empty_') return '';
+			return chunk;
+		})
+		.map(chunk => {
+			while(explodeChunkVariables(chunk).length > 0) {
+				chunk = explodeChunkVariables(chunk);
+			}
+			return chunk;
+		});
+
+	array = [].concat.apply([], array);
+	console.log(array);
+
+	let arrayWithProps = array
 		.map(chunk => {
 			let props = {};
 			let propArray = [];
@@ -22,11 +67,10 @@ export default (str) => {
 				});
 			}
 
-			if(chunk === '_empty_') chunk = '';
-
 			return {
 				value: chunk,
 				props: props
 			};
 		});
+	return arrayWithProps;
 };
