387f335e729fba1cd197d90b62995f5fd5b5eeaa
Lau Skeeter <lawr@lauragonzalez.cc>
2017-03-16 13:00:25 +0100

    oops, bunny

--- src/lib/random.js
+++ src/lib/random.js
@@ -20,7 +20,7 @@ const randomNumber = (key,seed=makeSeed()) => {
 };
 
 const makeSeed = () => {
-	return (Date.now()*Math.random())+"-"+Date.now();
+	return (Date.now()*Math.random())+'-'+Date.now();
 };
 
 const mangleSeed = (seed) => {
